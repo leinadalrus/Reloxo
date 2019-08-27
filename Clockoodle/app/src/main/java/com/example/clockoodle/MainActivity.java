65eec5d3e8267bb34556661e1730440bb910f3aa
Daniel <48736455+daandaav@users.noreply.github.com>
2019-08-27 16:39:41 +1000

    MainActivity draft file
    This is just a draft which is meant to be a rework for the substitute activity (SubActivity & SubFrag). I am planning to make a working app demo by removing redundant code and implementing simpler code.

---
+++ Clockoodle/app/src/main/java/com/example/clockoodle/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.preloxo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+import com.example.clockoodle.PrelFrag.optionSelect;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    int hours, minutes;
+
+    public static final String main_tag = "MainActivity";
+    public static final String main_frag = "MainFragment";
+
+    // Initialize Alarm Activity Screen
+    public void init_alarm(String msg, int hrs, int mins) {
+        Intent init_alarm_intent = new Intent (AlarmClock.ACTION_SET_ALARM)
+            .putExtra(AlarmClock.EXTRA_MESSAGE, msg)
+            .putExtra(AlarmClock.EXTRA_HOUR, hrs)
+            .putExtra(AlarmClock.EXTRA_MINUTES, mins);
+
+            if (intent.resolveActivity(getPackageManager())!=null) {
+                startActivity(init_alarm_intent);
+            }
+    }
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.Layout.MainActivity);
+
+        if (getSupportFragmentManager().findFragmentByTag(main_tag) == null) {
+            SubFrag subFrag = new SubFrag();
+            FragmentTransaction init_SubFragTransaction = getSupportFragmentManager().beginTransaction();
+            init_SubFragTransaction.add(main_frag, alarm_frag);
+            init_SubFragTransaction.commit();
+        } // Creates a new instance of the FragmentManager to allow for the MainFrag class to link to MainActivity and operate.
+
+        // OnClickListener : Alarm Set Button
+        public void button_set_alarm(Button button_btns) {
+            button_btns = findViewById(R.id.button_set_alarm_time);
+            button_btns.setOnClickOnListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Intent button_press = new Intent();
+                    startActivity(button_press);
+                    getUser_set_alarm();
+                    optionSelect();
+                }
+            });
+        }
+    }
+
+}
