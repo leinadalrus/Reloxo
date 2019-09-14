ed0ee7cd030db53f7761c3fbac72428b0e0ea0c9
Daandaav <danis5k6@gmail.com>
2019-09-14 10:03:43 +1000

    Activity Files

---
+++ Reloxo/app/src/main/java/com/example/AlarmActivity.java
@@ -0,0 +1,95 @@
+package com.example.reloxo;
+
+import android.provider.AlarmClock;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.os.Message;
+import android.os.Handler;
+
+public class AlarmActivity extends FragmentActivity {
+
+    static int alarm_TypeInt;
+
+    public static final String alarm_tag = "AlarmActivity";
+    public final String EXTRA_INTENT = "com.example.reloxo.MainActivity.EXTRA_INTENT";
+    public final String EXTRA_FRAGMENT = "com.example.reloxo.AlarmActivity.EXTRA_FRAGMENT";
+
+    Handler handler; // Handler variable for thread messaging.
+
+    // @Override create all new instances of values when app is re/started
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+        AlarmManager alarmManager;
+        Intent aIntentAct= new Intent(this, MainActivity.class);
+        PendingIntent pendIntent = PendingIntent.getBroadcast(this, 0, aIntentAct, 0);
+
+        if (getSupportFragmentManager().findFragmentByTag(alarm_tag) == null) {
+            AlarmFragment alarm_frag = new AlarmFragment();
+            FragmentTransaction alarmFragment_FragmentTransaction = getSupportFragmentManager().beginTransaction();
+            alarmFragment_FragmentTransaction.add(alarm_frag, alarm_tag);
+            alarmFragment_FragmentTransaction.commit();
+        } // Creates a new instance of the FragmentManager to allow for the AlarmFragment class to link to AlarmActivity and operate.
+
+        for (int i = 0; i < 20; i++) {
+            this.handler.post(new Thread() {
+                void msgRunner(Message msg) {
+                    if (msg.what == 0) {
+                        Bundle b = msg.getData();
+                        button_set();
+                    }
+                }
+            });
+        }   // Thread Message Handler, loops under 20 times, updating received data,
+            // while setting alarm.
+
+        // OnClickListener : Alarm Set Button
+        Button button_SetAlarm;
+        button_SetAlarm = findViewById(R.id.button_set);
+        button_SetAlarm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // TODO Auto-generated method stub
+                Intent button_Intent = new Intent();
+                startActivity(button_Intent);
+                button_set();
+            }
+        });
+
+        Button button_StopAlarm;
+        button_StopAlarm = findViewById(R.id.button_stop);
+        button_StopAlarm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent button_Intent = new Intent();
+                startActivity(button_Intent);
+                button_stop();
+            }
+        });
+    }
+
+    AlarmFragment.button_set button_set, button_set_operation;
+    com.example.reloxo.AlarmFragment.button_set button_set() {
+        button_set = button_set_operation;
+        return button_set_operation;
+    }
+
+    AlarmFragment.button_stop button_stop, button_stop_operation;
+    com.example.reloxo.AlarmFragment.button_stop button_stop() {
+        button_stop = button_stop_operation;
+        return button_stop_operation;
+    }
+
+}
---
+++ Reloxo/app/src/main/java/com/example/AlarmFragment.java
@@ -0,0 +1,67 @@
+package com.example.reloxo;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.os.SystemClock;
+import android.os.Handler;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.MenuItem;
+
+import androidx.fragment.app.Fragment;
+
+public class AlarmFragment extends Fragment {
+    AlarmManager alarmFragment_AlarmManager;
+    Intent alarmIntent = new Intent(getActivity(), AlarmActivity.class);
+    PendingIntent pendIntent = PendingIntent.getActivity(getActivity(), 0, alarmIntent, 0);
+    // PendingIntent allow outsider/foreign apps to have access to the OS's public code.
+    // getActivity() can be replaced by getActivities() if you have an array of Intents:
+    // getActivities(Context context, int requestCode, Intent[] intents, int flags)
+
+    Handler handler; // Thread Messaging Handler (with Looper looper capabilities).
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        /*
+         * Bundler bund // (bundle variable) needs to be initiated
+         * bund // Needs to be called into class button_set{}
+         * bund // Must receive and update AlarmManager data
+         */
+    }
+
+    public class button_set {
+        MenuItem primary_AlarmItem;
+        {
+
+            if (alarmFragment_AlarmManager == null) {
+
+                if (primary_AlarmItem.getItemId() == R.id.button_set) {
+
+                    AlarmActivity.alarm_TypeInt = AlarmManager.ELAPSED_REALTIME_WAKEUP;
+                    int ms_SecondDelay = 30000;
+                    alarmFragment_AlarmManager.setInexactRepeating(AlarmActivity.alarm_TypeInt, SystemClock.elapsedRealtime() + ms_SecondDelay,
+                            ms_SecondDelay, pendIntent);
+
+                    alarmFragment_AlarmManager = (AlarmManager) getActivity().getSystemService(getActivity().ALARM_SERVICE);
+                    // You may notice that tha AlarmManager typename is in brackets after an assignment operator.
+                    // This is called typecasting and it works as a force typesetter just for this specific assignment
+                    // to allow for the use of AlarmManager ready-built functions.
+                } else if (alarmFragment_AlarmManager != null) alarmFragment_AlarmManager.cancel(pendIntent);
+            }
+        }
+    }
+
+    public class button_stop {
+        MenuItem secondary_AlarmItem;
+        {
+            if (secondary_AlarmItem.getItemId() == R.id.button_stop) {
+                if (alarmFragment_AlarmManager != null)
+                    alarmFragment_AlarmManager.cancel(pendIntent);
+            }
+        }
+    }
+}
